22cb468fcec45fe4bb313c6a0b437e7ffe7fab78
Hardeyemmy <iiadeyemi307@gmail.com>
2026-08-10 23:03:42 +0100

    hero section ongoing

---
+++ lib/features/public/home/widgets/hero_welcome.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../../../../app/theme/app_colors.dart';
+
+class HeroSection extends StatelessWidget {
+  const HeroSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 80),
+    );
+  }
+}
